05f74d5c8b04594d1e857f90bcb8f9be97866cf3
trisula-digiart <kanzalistianm@gmail.com>
2026-07-23 21:43:55 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -8,7 +8,7 @@ window.closeAuthModal = function() {
   if (modal) modal.classList.add('hidden');
 };
 
-window.switchAuthTab = function(type) {
+window.switchAuthTab = function(type) { 
   const custBtn = document.getElementById('tab-auth-customer');
   const staffBtn = document.getElementById('tab-auth-staff');
   const custForm = document.getElementById('auth-customer-form');
